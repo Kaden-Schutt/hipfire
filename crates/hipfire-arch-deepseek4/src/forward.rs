HUSRCF/hipscope

perf(deepseek4): barrier-free MoE grouped WMMA kernel (+39-43%)
Eliminate __syncthreads() from the MoE grouped WMMA K-group loop.
Each warp now loads X data directly from global memory instead of
cooperative LDS staging. At ~4.8% memory BW utilization the 4×
redundant global reads are free; the win comes from eliminating
wave-level serialization at the syncthreads barrier.

Previously the 4 waves per WG rendezvoused 2× per K-group
(32 barriers per dispatch). With only 1 WG/CU at 128 threads/WG,
waves spin-waited at the barrier — the fast waves stall for the
slowest wave. Raising occupancy (96 VGPRs experiment) gave 25%
more waves but 0% BW gain, confirming the barrier was the real
bottleneck.

The nosync kernel leaves the LDS X buffer unused and reads X from
global memory on demand in the inner K-tile loop. Same codebook,
pack layout, bilinear DQ, and WMMA tile shape — only the load
schedule for X data changes. Bit-exact output.

Measured on gfx1151 (Radeon 8060S, 40 CUs) with
deepseek-v4-flash.mq2lloyd (88B MQ2-Lloyd), pp_batch=1024:

  ctx | baseline | nosync  | Δ
  128 | 17.5     | 25.0    | +43%
  256 | 13.5     | 19.3    | +43%
  512 | 10.7     | 15.2    | +42%
 1024 |  8.8     | 12.2    | +39%

Gate: HIPFIRE_DEEPSEEK4_MOE_NOSYNC=1 (opt-in for now).

Assisted-by: OpenCode:deepseek/deepseek-v4-flash

crates/hipfire-arch-deepseek4/src/forward.rs
@@ -6639,7 +6639,24 @@ fn ffn_batched(
         // MMQ-style preload (default ON for 4w; opt out via =0).
         let use_mmqload = use_lloyd_4w && std::env::var("HIPFIRE_DEEPSEEK4_MOE_MMQLOAD")
             .as_deref() != Ok("0");
-        if use_mmqload {
+        // Barrier-free nosync variant (opt in via =1).
+        let use_nosync = use_mmqload && std::env::var("HIPFIRE_DEEPSEEK4_MOE_NOSYNC")
+            .as_deref() == Ok("1");
+        if use_nosync {
+            gpu.gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_mmqload_nosync(
+                gate_up_ptrs,
+                &pbs.moe_expert_tile_ids,
+                &pbs.moe_sorted_slot_index,
+                &pbs.ffn_x_rot_batch,
+                &pbs.moe_y_gate_up_grouped,
+                2 * im,
+                hidden,
+                k_top,
+                m_total_max,
+                batch_size,
+            )
+            .map_err(|e| format!("gemm_mq2g256_lloyd_moe_grouped_nosync gate_up l{layer_idx}: {e:?}"))?;
+        } else if use_mmqload {
             gpu.gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_mmqload(
                 gate_up_ptrs,
                 &pbs.moe_expert_tile_ids,
@@ -6755,7 +6772,23 @@ fn ffn_batched(
           && im % 256 == 0;
         let use_mmqload_down = use_lloyd_4w_down && std::env::var("HIPFIRE_DEEPSEEK4_MOE_MMQLOAD")
             .as_deref() != Ok("0");
-        if use_mmqload_down {
+        let use_nosync_down = use_mmqload_down && std::env::var("HIPFIRE_DEEPSEEK4_MOE_NOSYNC")
+            .as_deref() == Ok("1");
+        if use_nosync_down {
+            gpu.gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_mmqload_nosync(
+                w2_ptrs,
+                &pbs.moe_expert_tile_ids,
+                &pbs.moe_sorted_slot_index,
+                &pbs.moe_rot_batch,
+                &pbs.moe_y_down_grouped,
+                hidden,
+                im,
+                1,
+                m_total_max,
+                batch_size * k_top,
+            )
+            .map_err(|e| format!("gemm_mq2g256_lloyd_moe_grouped_nosync down l{layer_idx}: {e:?}"))?;
+        } else if use_mmqload_down {
             gpu.gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_mmqload(
                 w2_ptrs,
                 &pbs.moe_expert_tile_ids,

crates/rdna-compute/src/dispatch.rs
@@ -30526,6 +30526,74 @@ impl Gpu {
         result
     }
 
+    /// Barrier-free variant of the mmqload kernel.
+    /// Eliminates __syncthreads() and LDS X staging.
+    pub fn gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_mmqload_nosync(
+        &mut self,
+        expert_weight_ptrs: &GpuTensor,
+        expert_tile_ids: &GpuTensor,
+        sorted_slot_index: &GpuTensor,
+        x_src: &GpuTensor,
+        y_grouped: &GpuTensor,
+        m: usize,
+        k: usize,
+        x_row_div: usize,
+        m_total: usize,
+        x_src_rows: usize,
+    ) -> HipResult<()> {
+        self.bind_thread()?;
+        debug_assert_eq!(m % 64, 0, "gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_mmqload_nosync: M must be a multiple of 64 (got {m})");
+        debug_assert_eq!(k % 256, 0, "gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_mmqload_nosync: K must be a multiple of 256 (got {k})");
+        let kernel_name = "gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_mmqload_nosync";
+        let kernel_src  = kernels::GEMM_MQ2G256_LLOYD_MOE_GROUPED_WMMA_4W_K2_MMQLOAD_NOSYNC_SRC;
+        self.ensure_kernel(kernel_name, kernel_src, kernel_name)?;
+        let x_f16_ptr = self.ensure_fp16_x(x_src, x_src_rows * k)?;
+
+        let ep = expert_weight_ptrs.buf.as_ptr();
+        let tp = expert_tile_ids.buf.as_ptr();
+        let sp = sorted_slot_index.buf.as_ptr();
+        let xp = x_f16_ptr;
+        let yp = y_grouped.buf.as_ptr();
+        let m_val   = m as i32;
+        let k_val   = k as i32;
+        let xrd_val = x_row_div as i32;
+        let mt_val  = m_total as i32;
+
+        let mut params: Vec<*mut c_void> = vec![
+            &ep as *const _ as *mut c_void,
+            &tp as *const _ as *mut c_void,
+            &sp as *const _ as *mut c_void,
+            &xp as *const _ as *mut c_void,
+            &yp as *const _ as *mut c_void,
+            &m_val as *const _ as *mut c_void,
+            &k_val as *const _ as *mut c_void,
+            &xrd_val as *const _ as *mut c_void,
+            &mt_val as *const _ as *mut c_void,
+        ];
+
+        let row_tiles  = ((m + 63) / 64) as u32;
+        let slot_tiles = ((m_total + 15) / 16) as u32;
+        let mq2_weight_bytes = m * (k / 256) * 72;
+        let bytes = m_total * k * 2 + (m_total * m) * 4 + mq2_weight_bytes;
+        let timer = crate::profile::begin_timer(
+            &self.hip, "gemm", kernel_name, bytes,
+        );
+        let result = self.launch_maybe_blob(
+            kernel_name,
+            [row_tiles, slot_tiles, 1], [128, 1, 1], 0, &mut params,
+            || {
+                let mut b = hip_bridge::KernargBlob::new();
+                b.push_ptr(ep); b.push_ptr(tp); b.push_ptr(sp);
+                b.push_ptr(xp); b.push_ptr(yp);
+                b.push_i32(m_val); b.push_i32(k_val);
+                b.push_i32(xrd_val); b.push_i32(mt_val);
+                b
+            },
+        );
+        if let Some(t) = timer { t.finish(&self.hip); }
+        result
+    }
+
     /// y = A_q8_0 * x (quantized GEMV for Q8_0)
     /// F16-weight × F32-input GEMV. y[m] = W_f16[m, k] @ x_f32[k].
     /// Keeps full F32 input precision — use this for full-precision F16

Kaden-Schutt/hipfire

crates/hipfire-arch-deepseek4/src/forward.rs
@@ -6636,7 +6636,24 @@ fn ffn_batched(
             _ => (gpu.arch.starts_with("gfx11") || gpu.arch.starts_with("gfx12"))
         } && (2 * im) % 64 == 0
           && hidden % 256 == 0;
-        if use_lloyd_4w {
+        // Barrier-free nosync variant (opt in via =1).
+        let use_nosync = use_lloyd_4w && std::env::var("HIPFIRE_DEEPSEEK4_MOE_NOSYNC")
+            .as_deref() == Ok("1");
+        if use_nosync {
+            gpu.gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_nosync(
+                gate_up_ptrs,
+                &pbs.moe_expert_tile_ids,
+                &pbs.moe_sorted_slot_index,
+                &pbs.ffn_x_rot_batch,
+                &pbs.moe_y_gate_up_grouped,
+                2 * im,
+                hidden,
+                k_top,
+                m_total_max,
+                batch_size,
+            )
+            .map_err(|e| format!("gemm_mq2g256_lloyd_moe_grouped_nosync gate_up l{layer_idx}: {e:?}"))?;
+        } else if use_lloyd_4w {
             gpu.gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2(
                 gate_up_ptrs,
                 &pbs.moe_expert_tile_ids,
@@ -6736,7 +6753,23 @@ fn ffn_batched(
             _ => (gpu.arch.starts_with("gfx11") || gpu.arch.starts_with("gfx12"))
         } && hidden % 64 == 0
           && im % 256 == 0;
-        if use_lloyd_4w_down {
+        let use_nosync_down = use_lloyd_4w_down && std::env::var("HIPFIRE_DEEPSEEK4_MOE_NOSYNC")
+            .as_deref() == Ok("1");
+        if use_nosync_down {
+            gpu.gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_nosync(
+                w2_ptrs,
+                &pbs.moe_expert_tile_ids,
+                &pbs.moe_sorted_slot_index,
+                &pbs.moe_rot_batch,
+                &pbs.moe_y_down_grouped,
+                hidden,
+                im,
+                1,
+                m_total_max,
+                batch_size * k_top,
+            )
+            .map_err(|e| format!("gemm_mq2g256_lloyd_moe_grouped_nosync down l{layer_idx}: {e:?}"))?;
+        } else if use_lloyd_4w_down {
             gpu.gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2(
                 w2_ptrs,
                 &pbs.moe_expert_tile_ids,

crates/rdna-compute/src/dispatch.rs
@@ -30458,6 +30458,73 @@ impl Gpu {
         result
     }
 
+    /// Barrier-free MoE grouped WMMA kernel. Eliminates __syncthreads()
+    /// and LDS X staging. Each wave loads X directly from global memory.
+    pub fn gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_nosync(
+        &mut self,
+        expert_weight_ptrs: &GpuTensor,
+        expert_tile_ids: &GpuTensor,
+        sorted_slot_index: &GpuTensor,
+        x_src: &GpuTensor,
+        y_grouped: &GpuTensor,
+        m: usize,
+        k: usize,
+        x_row_div: usize,
+        m_total: usize,
+        x_src_rows: usize,
+    ) -> HipResult<()> {
+        self.bind_thread()?;
+        debug_assert_eq!(m % 64, 0, "gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_nosync: M must be a multiple of 64 (got {m})");
+        debug_assert_eq!(k % 256, 0, "gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_nosync: K must be a multiple of 256 (got {k})");
+        let kernel_name = "gemm_mq2g256_lloyd_moe_grouped_wmma_4w_k2_nosync";
+        let kernel_src  = kernels::GEMM_MQ2G256_LLOYD_MOE_GROUPED_WMMA_4W_K2_NOSYNC_SRC;
+        self.ensure_kernel(kernel_name, kernel_src, kernel_name)?;
+        let x_f16_ptr = self.ensure_fp16_x(x_src, x_src_rows * k)?;
+
+        let ep = expert_weight_ptrs.buf.as_ptr();
+        let tp = expert_tile_ids.buf.as_ptr();
+        let sp = sorted_slot_index.buf.as_ptr();
+        let xp = x_f16_ptr;
+        let yp = y_grouped.buf.as_ptr();
+        let m_val   = m as i32;
+        let k_val   = k as i32;
+        let xrd_val = x_row_div as i32;
+        let mt_val  = m_total as i32;
+
+        let mut params: Vec<*mut c_void> = vec![
+            &ep as *const _ as *mut c_void,
+            &tp as *const _ as *mut c_void,
+            &sp as *const _ as *mut c_void,
+            &xp as *const _ as *mut c_void,
+            &yp as *const _ as *mut c_void,
+            &m_val as *const _ as *mut c_void,
+            &k_val as *const _ as *mut c_void,
+            &xrd_val as *const _ as *mut c_void,
+            &mt_val as *const _ as *mut c_void,
+        ];
+
+        let row_tiles  = ((m + 63) / 64) as u32;
+        let slot_tiles = ((m_total + 15) / 16) as u32;
+        let mq2_weight_bytes = m * (k / 256) * 72;
+        let bytes = m_total * k * 2 + (m_total * m) * 4 + mq2_weight_bytes;
+        let timer = crate::profile::begin_timer(
+            &self.hip, "gemm", kernel_name, bytes,
+        );
+        let result = self.launch_maybe_blob(
+            kernel_name,
+            [row_tiles, slot_tiles, 1], [128, 1, 1], 0, &mut params,
+            || {
+                let mut b = hip_bridge::KernargBlob::new();
+                b.push_ptr(ep); b.push_ptr(tp); b.push_ptr(sp);
+                b.push_ptr(xp); b.push_ptr(yp);
+                b.push_i32(m_val); b.push_i32(k_val);
+                b.push_i32(xrd_val); b.push_i32(mt_val);
+                b
+            },
+        );
+        if let Some(t) = timer { t.finish(&self.hip); }
+        result
+    }
     /// y = A_q8_0 * x (quantized GEMV for Q8_0)
     /// F16-weight × F32-input GEMV. y[m] = W_f16[m, k] @ x_f32[k].
     /// Keeps full F32 input precision — use this for full-precision F16